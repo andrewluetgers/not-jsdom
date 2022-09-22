bc866ddc20d14d16d37849e3721ccc8ed3e5ea6f
andrew luetgers <andrew.luetgers@gmail.com>
2022-09-21 23:48:23 -0500

    basic impl

---
+++ index.js
@@ -0,0 +1,12 @@
+let parseHTML = require('linkedom'),
+	VirtualConsole = require('./virtual-console')
+
+
+function JSDOM(html, options) {
+	return parseHTML(html)
+}
+
+module.exports = {
+	JSDOM,
+	VirtualConsole
+}
